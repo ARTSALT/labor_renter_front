2523c2ea328fc9f5462993bd094e35e4426b4a15
Ricardo <ricardomarinho638@gmail.com>
2025-01-29 16:20:52 -0300

    more config

---
+++ labor_renter_front/app/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <main className="main-content">
+        <h2>Página Inicial</h2>
+        <p></p>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
